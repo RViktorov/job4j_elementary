b4871498278ee6ea8d1b45bb79daa8274a58826b
Roman Viktorov <rpviktorov@gmail.com>
2024-01-13 15:35:51 +0300

    fix1: 9. Площадь треугольника. [#175474 #472375]

--- src/main/java/ru/job4j/condition/TriangleArea.java
+++ src/main/java/ru/job4j/condition/TriangleArea.java
@@ -2,9 +2,9 @@ package ru.job4j.condition;
 
 public class TriangleArea {
     public static double area(double a, double b, double c) {
-        double semiperimeterTriangle = (a + b + c) / 2;
-        double result = Math.sqrt(semiperimeterTriangle * (semiperimeterTriangle - a) * (semiperimeterTriangle - b)
-                * (semiperimeterTriangle - c));
+        double p = (a + b + c) / 2;
+        double result = Math.sqrt(p * (p - a) * (p - b)
+                * (p - c));
         return result;
     }
 
