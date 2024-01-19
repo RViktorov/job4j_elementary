4c84f4c4b19a96f6c5f5b991199b99bb66091a47
Roman Viktorov <rpviktorov@gmail.com>
2024-01-19 21:14:09 +0300

    fix: 5.7. Ипотека [#156315 #474770]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -2,14 +2,11 @@ package ru.job4j.loop;
 
 public class Mortgage {
 
-    public Mortgage() {
-    }
-
     public static int year(double amount, int salary, double percent) {
         int year = 0;
 
         while (amount > 0) {
-            amount = amount + (amount * (percent / 100)) - salary;
+            amount += amount * percent / 100 - salary;
             year++;
         }
         return year;
